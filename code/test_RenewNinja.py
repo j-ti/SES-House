284882ebc8c09088d0febdac4929b08a98991c6d
Joris Clement <j.clement@campus.tu-berlin.de>
2019-12-07 21:09:03 +0100

    Include wind powers into simple model
    Therfore add a function that reads the sample wind file.

--- code/test_RenewNinja.py
+++ code/test_RenewNinja.py
@@ -1,10 +1,18 @@
 import unittest
 from datetime import datetime
 
-from RenewNinja import getSamplePvApi, getSamplePv
+from RenewNinja import getSamplePvApi, getSamplePv, getSampleWind
 
 
 class Test(unittest.TestCase):
+    def testGetSampleWind(self):
+        data = getSampleWind(
+            datetime(2014, 1, 1, 0, 0, 0), datetime(2014, 1, 1, 23, 59, 59)
+        )
+        self.assertEqual(len(data), 24)
+        for electricity in data:
+            self.assertGreaterEqual(electricity, 0)
+
     def testGetSamplePv(self):
         data = getSamplePv(
             datetime(2014, 1, 1, 0, 0, 0), datetime(2014, 1, 1, 23, 59, 59)
