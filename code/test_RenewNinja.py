51bd1a5934975e2664c19d8148caea84dc413627
Joris Clement <j.clement@campus.tu-berlin.de>
2019-12-07 18:42:45 +0100

    Add function to read pv sample file

--- code/test_RenewNinja.py
+++ code/test_RenewNinja.py
@@ -1,10 +1,18 @@
 import unittest
 from datetime import datetime
 
-from RenewNinja import getSamplePvApi
+from RenewNinja import getSamplePvApi, getSamplePv
 
 
 class Test(unittest.TestCase):
+    def testGetSamplePv(self):
+        data = getSamplePv(
+            datetime(2014, 1, 1, 0, 0, 0), datetime(2014, 1, 1, 23, 59, 59)
+        )
+        self.assertEqual(len(data), 24)
+        for electricity in data:
+            self.assertGreaterEqual(electricity, 0)
+
     def testGetSamplePvApi(self):
         metadata, data = getSamplePvApi(datetime(2014, 1, 1), datetime(2014, 1, 1))
         self.assertEqual(len(data), 24)
