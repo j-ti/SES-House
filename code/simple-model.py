a9ca484ef4c4d50c85fbea3c7cd298db6860044a
jtm91 <jtm91@mailbox.tu-berlin.de>
2019-12-09 12:35:01 +0100

    added init values, checked constraints, and black formatted

--- code/simple-model.py
+++ code/simple-model.py
@@ -3,6 +3,15 @@
 import gurobipy as gp
 from gurobipy import GRB
 
+## Initialization
+# Battery init (to be moved to a initialization file)
+SOCmin = 0.1
+SOCinit = 0.5
+SOCmax = 0.9
+E_bat_max = 10  # 1 kWh
+eta = 0.9  # charging efficientcy 90%
+P_bat_max = 3
+
 pvGenerators = ["pv1", "pv2"]
 dieselGenerators = ["dg1"]
 windGenerators = ["wind1"]
@@ -17,7 +26,9 @@ loads = ["uncontrollable"] + shiftableLoads + interrupableLoads
 
 pvPowers = {"pv1": [1, 3, 0], "pv2": [0, 0, 2]}
 windPowers = {"wind1": [1, 3, 0], "wind2": [0, 0, 2]}
+batPowersTest = {"bat1": [1, -2, 1]}
 times = [1575375592443, 1575375592449, 1575375593949]
+step = 1 # one time step is one hour
 
 assert len(pvPowers) == len(pvGenerators)
 for value in pvPowers.values():
@@ -33,8 +44,34 @@ gridVars = m.addVars(len(times), 1, vtype=GRB.CONTINUOUS, name="gridPowers")
 dieselGeneratorsVars = m.addVars(
     len(times), 1, vtype=GRB.CONTINUOUS, name="dieselGenerators"
 )
+batteryPowerVars = m.addVars(
+    len(times), 1, lb=-P_bat_max, ub=P_bat_max, vtype=GRB.CONTINUOUS, name="batPowers"
+)
+batteryEnergyVars = m.addVars(
+    len(times), 1, lb=SOCmin*E_bat_max, ub=SOCmax*E_bat_max, vtype=GRB.CONTINUOUS, name="batEnergys"
+)
 
 m.setObjective(gp.quicksum(dieselGeneratorsVars) + gp.quicksum(gridVars), GRB.MINIMIZE)
+
+# Manually specify the (dis-)charging power (has to be optimized by the price later on)
+m.addConstrs(
+    (batteryPowerVars[i, 0] == batPowersTest["bat1"][i] for i in range(len(times))),
+    "battery power",
+)
+
+# A battery charges when the 'batteryPowerVars' value is negative and discharging otherwise
+m.addConstrs(
+    (
+        batteryEnergyVars[i + 1, 0]
+        == batteryEnergyVars[i, 0] - eta * batteryPowerVars[i, 0] * step
+        for i in range(len(times) - 1)
+    ),
+    "battery charging",
+)
+
+m.addConstr(
+    (batteryEnergyVars[0, 0] == SOCinit * E_bat_max), "battery init",
+)
 
 m.addConstrs(
     (gridVars[i, 0] >= 0 for i in range(len(times))), "grid positive",
@@ -63,6 +100,7 @@ m.addConstrs(
         + pvVars[i, 0]
         + windVars[i, 0]
         + dieselGeneratorsVars[i, 0]
+        + batteryPowerVars[i, 0]
         - fixedLoadVars[i, 0]
         == 0
         for i in range(len(times))
@@ -74,5 +112,5 @@ m.optimize()
 
 for v in m.getVars():
     print("%s %g" % (v.varName, v.x))
-
+9
 print("Obj: %g" % m.objVal)
