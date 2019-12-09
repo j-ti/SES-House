ff7cb62556504de60fa58fe43d55117bf9c05df9
QiyiLiu71 <QiyiLiu71@users.noreply.github.com>
2019-12-09 22:57:42 +0100

    add price sample and still working on ev.....

--- code/simple-model.py
+++ code/simple-model.py
@@ -4,20 +4,35 @@ from datetime import datetime, timedelta
 from util import constructTimeStamps
 
 from load_loads import getLoadsData
+from load_price import getPriceData
+
 from RenewNinja import getSamplePv, getSampleWind
 
 import gurobipy as gp
 from gurobipy import GRB
 
+import numpy
 
 # Initialization
 # Battery init (to be moved to a initialization file)
-SOCmin = 0.1
-SOCinit = 0.5
-SOCmax = 0.9
+SOC_bat_min = 0.1
+SOC_bat_init = 0.5
+SOC_bat_max = 0.9
 E_bat_max = 10  # 1 kWh
-eta = 0.9  # charging efficientcy 90%
+eta_bat = 0.9  # charging efficientcy 90%
 P_bat_max = 3
+#EV init
+SOC_ev_min=0.2
+SOC_ev_init=0.1
+SOC_ev_max=0.9
+P_ev_max=3
+E_ev_max=10
+eta_ev=0.9
+t_a_ev=datetime(2014, 1, 1, 1, 0, 0)
+t_b_ev=datetime(2014, 1, 1, 9, 0, 0)
+t_goal_ev=datetime(2014, 1, 1, 6, 0, 0)
+
+
 
 pvGenerators = ["pv1", "pv2"]
 dieselGenerators = ["dg1"]
@@ -34,9 +49,12 @@ loads = ["uncontrollable"] + shiftableLoads + interrupableLoads
 start = datetime(2014, 1, 1, 0, 0, 0)
 end = datetime(2014, 1, 1, 23, 59, 59)
 stepsize = timedelta(hours=1)
+
 times = constructTimeStamps(start, end, stepsize)
 batPowersTest = {"bat1": [1, -2, 1]}
 
+CostDiesel=0.2
+CostGrid=getPriceData("../sample/pecan-iso_neiso-day_ahead_lmp_avg-20190602.csv", start, end, stepsize=stepsize)
 m = gp.Model("simple-model")
 
 pvVars = m.addVars(
@@ -59,29 +77,61 @@ dieselGeneratorsVars = m.addVars(
 batteryPowerVars = m.addVars(
     len(times), 1, lb=-P_bat_max, ub=P_bat_max, vtype=GRB.CONTINUOUS, name="batPowers"
 )
+evPowerVars = m.addVars(
+    len(times), 1, lb=-P_ev_max, ub=P_ev_max, vtype=GRB.CONTINUOUS, name="evPowers"
+)
+
 batteryEnergyVars = m.addVars(
     len(times),
     1,
-    lb=SOCmin * E_bat_max,
-    ub=SOCmax * E_bat_max,
+    lb=SOC_bat_min * E_bat_max,
+    ub=SOC_bat_max * E_bat_max,
     vtype=GRB.CONTINUOUS,
     name="batEnergys",
 )
+evEnergyVars = m.addVars(
+    len(times),
+    1,
+    lb=SOC_ev_min * E_ev_max,
+    ub=SOC_ev_max * E_ev_max,
+    vtype=GRB.CONTINUOUS,
+    name="evEnergys",
+)
 
-m.setObjective(gp.quicksum(dieselGeneratorsVars) + gp.quicksum(gridVars), GRB.MINIMIZE)
+
+m.setObjective(CostDiesel*gp.quicksum(dieselGeneratorsVars) + gp.quicksum(gridVars), GRB.MINIMIZE)
 
 # A battery charges when the 'batteryPowerVars' value is negative and discharging otherwise
 m.addConstrs(
     (
         batteryEnergyVars[i + 1, 0]
         == batteryEnergyVars[i, 0]
-        - eta * batteryPowerVars[i, 0] * 1  # stepsize: 1 hour
+        - eta_bat * batteryPowerVars[i, 0] * stepsize.total_seconds()/3600  # stepsize: 1 hour
         for i in range(len(times) - 1)
     ),
     "battery charging",
 )
+m.addConstrs(
+    (
+        evEnergyVars[i + 1, 0]
+        == evEnergyVars[i, 0]
+        - eta_ev * evPowerVars[i, 0] * stepsize.total_seconds()/3600  # stepsize: 1 hour
+        for i in range(len(times) - 1)
+    ),
+    "ev charging",
+)
 
-m.addConstr((batteryEnergyVars[0, 0] == SOCinit * E_bat_max), "battery init")
+m.addConstr((batteryEnergyVars[0, 0] == SOC_bat_init * E_bat_max), "battery init")
+m.addConstrs(
+    (evPowerVars[i, 0] == 0 for i in range(int((t_a_ev-start).total_seconds()/3600))),
+    "ev no charging before t_a",
+)
+m.addConstrs(
+    (evPowerVars[i, 0] == 0 for i in range(int((t_b_ev-start).total_seconds()/3600),int((end-t_b_ev).total_seconds()/3600))),
+    "ev no charging before t_a",
+)
+
+# m.addConstr((evEnergyVars[(t_a_ev-start).total_seconds()/3600, 0] == SOC_ev_init * E_ev_max), "ev init")
 
 m.addConstrs(
     (
@@ -89,6 +139,8 @@ m.addConstrs(
         + pvVars.sum([i, "*"])
         + windVars.sum([i, "*"])
         + dieselGeneratorsVars.sum([i, "*"])
+        # +batteryEnergyVars.sum([i,"*"])
+        # +evPowerVars.sum([i,"*"])
         == fixedLoadVars.sum([i, "*"])
         for i in range(len(times))
     ),
@@ -115,7 +167,7 @@ m.addConstrs(
 )
 
 loadValues = getLoadsData(
-    "./sample/pecan-home-grid_solar-manipulated.csv", start, end, stepsize
+    "../sample/pecan-home-grid_solar-manipulated.csv", start, end, stepsize
 )
 assert len(loadValues) == len(times)
 m.addConstrs(
