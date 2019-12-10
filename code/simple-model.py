b75a7f016661d09ce41ae0043d4dfae4a6b32514
QiyiLiu71 <QiyiLiu71@users.noreply.github.com>
2019-12-10 16:16:44 +0100

    Revert "Merge branch 'master' of https://github.com/j-ti/SES-House"
    This reverts commit 60b710ea67666328321460e966eb4d2ef5ac73d8, reversing
    changes made to c1a94fa533182fbbbd19f09014ca1d031a721681.

--- code/simple-model.py
+++ code/simple-model.py
@@ -11,6 +11,7 @@ from RenewNinja import getSamplePv, getSampleWind
 import gurobipy as gp
 from gurobipy import GRB
 
+import numpy
 
 # Initialization
 # Battery init (to be moved to a initialization file)
@@ -20,16 +21,17 @@ SOC_bat_max = 0.9
 E_bat_max = 10  # 1 kWh
 eta_bat = 0.9  # charging efficientcy 90%
 P_bat_max = 3
-# EV init
-SOC_ev_min = 0.2
-SOC_ev_init = 0.1
-SOC_ev_max = 0.9
-P_ev_max = 3
-E_ev_max = 10
-eta_ev = 0.9
-t_a_ev = datetime(2014, 1, 1, 1, 0, 0)
-t_b_ev = datetime(2014, 1, 1, 9, 0, 0)
-t_goal_ev = datetime(2014, 1, 1, 6, 0, 0)
+#EV init
+SOC_ev_min=0.2
+SOC_ev_init=0.1
+SOC_ev_max=0.9
+P_ev_max=3
+E_ev_max=10
+eta_ev=0.9
+t_a_ev=datetime(2014, 1, 1, 1, 0, 0)
+t_b_ev=datetime(2014, 1, 1, 9, 0, 0)
+t_goal_ev=datetime(2014, 1, 1, 6, 0, 0)
+
 
 
 pvGenerators = ["pv1", "pv2"]
@@ -49,14 +51,10 @@ end = datetime(2014, 1, 1, 23, 59, 59)
 stepsize = timedelta(hours=1)
 
 times = constructTimeStamps(start, end, stepsize)
+batPowersTest = {"bat1": [1, -2, 1]}
 
-CostDiesel = 0.2
-CostGrid = getPriceData(
-    "./sample/pecan-iso_neiso-day_ahead_lmp_avg-20190602.csv",
-    start,
-    end,
-    stepsize=stepsize,
-)
+CostDiesel=0.2
+CostGrid=getPriceData("../sample/pecan-iso_neiso-day_ahead_lmp_avg-20190602.csv", start, end, stepsize=stepsize)
 m = gp.Model("simple-model")
 
 pvVars = m.addVars(
@@ -77,20 +75,10 @@ dieselGeneratorsVars = m.addVars(
     name="dieselGenerators",
 )
 batteryPowerVars = m.addVars(
-    len(times),
-    len(nonEVBatteries),
-    lb=-P_bat_max,
-    ub=P_bat_max,
-    vtype=GRB.CONTINUOUS,
-    name="batPowers",
+    len(times), 1, lb=-P_bat_max, ub=P_bat_max, vtype=GRB.CONTINUOUS, name="batPowers"
 )
 evPowerVars = m.addVars(
-    len(times),
-    len(electricVehicles),
-    lb=-P_ev_max,
-    ub=P_ev_max,
-    vtype=GRB.CONTINUOUS,
-    name="evPowers",
+    len(times), 1, lb=-P_ev_max, ub=P_ev_max, vtype=GRB.CONTINUOUS, name="evPowers"
 )
 
 batteryEnergyVars = m.addVars(
@@ -111,19 +99,14 @@ evEnergyVars = m.addVars(
 )
 
 
-m.setObjective(
-    CostDiesel * gp.quicksum(dieselGeneratorsVars) + gp.quicksum(gridVars), GRB.MINIMIZE
-)
+m.setObjective(CostDiesel*gp.quicksum(dieselGeneratorsVars) + gp.quicksum(gridVars), GRB.MINIMIZE)
 
 # A battery charges when the 'batteryPowerVars' value is negative and discharging otherwise
 m.addConstrs(
     (
         batteryEnergyVars[i + 1, 0]
         == batteryEnergyVars[i, 0]
-        - eta_bat
-        * batteryPowerVars[i, 0]
-        * stepsize.total_seconds()
-        / 3600  # stepsize: 1 hour
+        - eta_bat * batteryPowerVars[i, 0] * stepsize.total_seconds()/3600  # stepsize: 1 hour
         for i in range(len(times) - 1)
     ),
     "battery charging",
@@ -132,10 +115,7 @@ m.addConstrs(
     (
         evEnergyVars[i + 1, 0]
         == evEnergyVars[i, 0]
-        - eta_ev
-        * evPowerVars[i, 0]
-        * stepsize.total_seconds()
-        / 3600  # stepsize: 1 hour
+        - eta_ev * evPowerVars[i, 0] * stepsize.total_seconds()/3600  # stepsize: 1 hour
         for i in range(len(times) - 1)
     ),
     "ev charging",
@@ -143,20 +123,11 @@ m.addConstrs(
 
 m.addConstr((batteryEnergyVars[0, 0] == SOC_bat_init * E_bat_max), "battery init")
 m.addConstrs(
-    (
-        evPowerVars[i, 0] == 0
-        for i in range(int((t_a_ev - start).total_seconds() / 3600))
-    ),
+    (evPowerVars[i, 0] == 0 for i in range(int((t_a_ev-start).total_seconds()/3600))),
     "ev no charging before t_a",
 )
 m.addConstrs(
-    (
-        evPowerVars[i, 0] == 0
-        for i in range(
-            int((t_b_ev - start).total_seconds() / 3600),
-            int((end - t_b_ev).total_seconds() / 3600),
-        )
-    ),
+    (evPowerVars[i, 0] == 0 for i in range(int((t_b_ev-start).total_seconds()/3600),int((end-t_b_ev).total_seconds()/3600))),
     "ev no charging before t_a",
 )
 
@@ -168,7 +139,7 @@ m.addConstrs(
         + pvVars.sum([i, "*"])
         + windVars.sum([i, "*"])
         + dieselGeneratorsVars.sum([i, "*"])
-        + batteryPowerVars.sum([i, "*"])
+        # +batteryEnergyVars.sum([i,"*"])
         # +evPowerVars.sum([i,"*"])
         == fixedLoadVars.sum([i, "*"])
         for i in range(len(times))
@@ -196,7 +167,7 @@ m.addConstrs(
 )
 
 loadValues = getLoadsData(
-    "./sample/pecan-home-grid_solar-manipulated.csv", start, end, stepsize
+    "../sample/pecan-home-grid_solar-manipulated.csv", start, end, stepsize
 )
 assert len(loadValues) == len(times)
 m.addConstrs(
