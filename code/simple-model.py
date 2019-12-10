fc1a95c2721078c7bf2a896e3518be5b0ae572fc
Joris Clement <j.clement@campus.tu-berlin.de>
2019-12-10 13:04:11 +0100

    Cleanup by 'black'

--- code/simple-model.py
+++ code/simple-model.py
@@ -21,17 +21,16 @@ SOC_bat_max = 0.9
 E_bat_max = 10  # 1 kWh
 eta_bat = 0.9  # charging efficientcy 90%
 P_bat_max = 3
-#EV init
-SOC_ev_min=0.2
-SOC_ev_init=0.1
-SOC_ev_max=0.9
-P_ev_max=3
-E_ev_max=10
-eta_ev=0.9
-t_a_ev=datetime(2014, 1, 1, 1, 0, 0)
-t_b_ev=datetime(2014, 1, 1, 9, 0, 0)
-t_goal_ev=datetime(2014, 1, 1, 6, 0, 0)
-
+# EV init
+SOC_ev_min = 0.2
+SOC_ev_init = 0.1
+SOC_ev_max = 0.9
+P_ev_max = 3
+E_ev_max = 10
+eta_ev = 0.9
+t_a_ev = datetime(2014, 1, 1, 1, 0, 0)
+t_b_ev = datetime(2014, 1, 1, 9, 0, 0)
+t_goal_ev = datetime(2014, 1, 1, 6, 0, 0)
 
 
 pvGenerators = ["pv1", "pv2"]
@@ -53,8 +52,13 @@ stepsize = timedelta(hours=1)
 times = constructTimeStamps(start, end, stepsize)
 batPowersTest = {"bat1": [1, -2, 1]}
 
-CostDiesel=0.2
-CostGrid=getPriceData("./sample/pecan-iso_neiso-day_ahead_lmp_avg-20190602.csv", start, end, stepsize=stepsize)
+CostDiesel = 0.2
+CostGrid = getPriceData(
+    "./sample/pecan-iso_neiso-day_ahead_lmp_avg-20190602.csv",
+    start,
+    end,
+    stepsize=stepsize,
+)
 m = gp.Model("simple-model")
 
 pvVars = m.addVars(
@@ -99,14 +103,19 @@ evEnergyVars = m.addVars(
 )
 
 
-m.setObjective(CostDiesel*gp.quicksum(dieselGeneratorsVars) + gp.quicksum(gridVars), GRB.MINIMIZE)
+m.setObjective(
+    CostDiesel * gp.quicksum(dieselGeneratorsVars) + gp.quicksum(gridVars), GRB.MINIMIZE
+)
 
 # A battery charges when the 'batteryPowerVars' value is negative and discharging otherwise
 m.addConstrs(
     (
         batteryEnergyVars[i + 1, 0]
         == batteryEnergyVars[i, 0]
-        - eta_bat * batteryPowerVars[i, 0] * stepsize.total_seconds()/3600  # stepsize: 1 hour
+        - eta_bat
+        * batteryPowerVars[i, 0]
+        * stepsize.total_seconds()
+        / 3600  # stepsize: 1 hour
         for i in range(len(times) - 1)
     ),
     "battery charging",
@@ -115,7 +124,10 @@ m.addConstrs(
     (
         evEnergyVars[i + 1, 0]
         == evEnergyVars[i, 0]
-        - eta_ev * evPowerVars[i, 0] * stepsize.total_seconds()/3600  # stepsize: 1 hour
+        - eta_ev
+        * evPowerVars[i, 0]
+        * stepsize.total_seconds()
+        / 3600  # stepsize: 1 hour
         for i in range(len(times) - 1)
     ),
     "ev charging",
@@ -123,11 +135,20 @@ m.addConstrs(
 
 m.addConstr((batteryEnergyVars[0, 0] == SOC_bat_init * E_bat_max), "battery init")
 m.addConstrs(
-    (evPowerVars[i, 0] == 0 for i in range(int((t_a_ev-start).total_seconds()/3600))),
+    (
+        evPowerVars[i, 0] == 0
+        for i in range(int((t_a_ev - start).total_seconds() / 3600))
+    ),
     "ev no charging before t_a",
 )
 m.addConstrs(
-    (evPowerVars[i, 0] == 0 for i in range(int((t_b_ev-start).total_seconds()/3600),int((end-t_b_ev).total_seconds()/3600))),
+    (
+        evPowerVars[i, 0] == 0
+        for i in range(
+            int((t_b_ev - start).total_seconds() / 3600),
+            int((end - t_b_ev).total_seconds() / 3600),
+        )
+    ),
     "ev no charging before t_a",
 )
 
