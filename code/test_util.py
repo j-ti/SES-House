fd5d0474835eebe30937c44adaa7f2d612718cd3
jtm91 <jtm91@mailbox.tu-berlin.de>
2019-12-16 15:42:12 +0100

    added Power constraint vor EV

--- code/test_util.py
+++ code/test_util.py
@@ -2,7 +2,7 @@ import unittest
 
 from datetime import datetime, timedelta
 
-from util import constructTimeStamps, getStepsize
+from util import constructTimeStamps, getStepsize, getTimeIndexRange
 
 
 class Test(unittest.TestCase):
@@ -31,6 +31,14 @@ class Test(unittest.TestCase):
         stepsize = timedelta(hours=1)
         timestamps = constructTimeStamps(start, end, stepsize)
         self.assertEqual(getStepsize(timestamps), stepsize)
+
+    def testTimeIndexRange(self):
+        start = datetime(2014, 1, 2, 0, 0, 0)
+        end = datetime(2014, 1, 2, 23, 0, 0)
+        stepsize = timedelta(hours=1)
+        timestamps = constructTimeStamps(start, end, stepsize)
+        indexList = getTimeIndexRange(timestamps, timestamps[0], timestamps[7])
+        self.assertEqual(indexList, [0, 1, 2, 3, 4, 5, 6])
 
 
 if __name__ == "__main__":
