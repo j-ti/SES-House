0e4d0f4e8722ed0163d471b2e07e063ae22c320b
loulouagr <agrofoglio@telecom-paristech.fr>
2020-01-05 16:36:02 +0100

    removing the bool value + cleaning the code

--- code/plotting.py
+++ code/plotting.py
@@ -21,13 +21,8 @@ def plotting(varName, varVal, soc_min_val, gridPrices, outputFolder, pieChart=Tr
             if val in varName[i]:
                 dico[val].append(varVal[i])
                 break
-    if pieChart:
-        plotting_pieChart(dico, outputFolder, gridPrices)
-    else:
-        plotting_1day(dico, outputFolder, gridPrices)
 
-
-def plotting_1day(dico, outputFolder, gridPrices):
+    # Plotting PV power, wind power and fixed loads power.
     plt.plot(dico["PVPowers"], label="pv", color="orange")
     plt.plot(dico["windPowers"], label="wind", color="blue")
     plt.plot(dico["fixedLoads"], label="Loads Power", color="red")
@@ -37,6 +32,7 @@ def plotting_1day(dico, outputFolder, gridPrices):
     plt.savefig(outputFolder + "/pv_wind-power.png")
     plt.show()
 
+    # Plotting EV and batteries energies
     plt.plot(dico["batEnergys"], label="Battery Energy", color="green")
     plt.plot(dico["evEnergys"], label="EV Energy", color="lime")
     plt.legend(loc="upper right", prop={"size": 8})
@@ -45,7 +41,7 @@ def plotting_1day(dico, outputFolder, gridPrices):
     plt.savefig(outputFolder + "/bat_ev-energy.png")
     plt.show()
 
-    # plt.plot([sum(x) for x in zip(dico["evPowers"],dico["batPowers"])], label="Battery + EV Power")
+    # Plotting all the powers from our system inside one graph
     plt.plot(dico["batPowers"], label="Battery Power", color="green")
     plt.plot(dico["evPowers"], label="EV Power", color="lime")
     plt.plot(dico["windPowers"], label="Wind Power", color="blue")
@@ -60,6 +56,7 @@ def plotting_1day(dico, outputFolder, gridPrices):
     plt.savefig(outputFolder + "/power-balance.png")
     plt.show()
 
+    # Plotting the evolution of the power in and out on the grid and the evolution of prices
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     ax1.plot(range(len(gridPrices)), gridPrices, label="Grid Price", color="gold")
@@ -73,41 +70,39 @@ def plotting_1day(dico, outputFolder, gridPrices):
     plt.savefig(outputFolder + "price - 1day.png")
     plt.show()
 
-
-# PVPowers": [],
-#         "windPowers": [],
-#         "batPowers": [],
-#         "batEnergys": [],
-#         "evPowers": [],
-#         "evEnergys": [],
-#         "fixedLoads": [],
-#         "gridPowers": [],
-#         "dieselGenerators": [],
-
-def plotting_pieChart(dico, outputFolder, gridPrices):
-    plot1 = [np.sum(dico["PVPowers"]), np.sum(dico["windPowers"]), np.sum(dico["gridPowers"]),
+    # Pie plot with the repartion of the power generated inside our system
+    plot1 = [np.sum(dico["PVPowers"]), np.sum(dico["windPowers"]), np.sum(dico["fromGridPowers"]),
              np.sum(dico["dieselGenerators"])]
-    plt.pie(plot1, labels=["PVPowers", "windPowers", "gridPowers", "dieselGenerators"])
+    plt.pie(plot1, labels=["PVPowers", "windPowers", "fromGridPowers", "dieselGenerators"])
     plt.title("Power given by the grid, pv, wind and diesel")
     plt.savefig(outputFolder + "/power_output_gen.png")
     plt.show()
 
-    plot2 = [np.sum(dico["batEnergys"]), np.sum(dico["evEnergys"])]
-    plt.pie(plot2, labels=["batEnergys", "evEnergys"])
-    plt.title("Energy inside the batteries")
-    plt.savefig(outputFolder + "/energy_batteries.png")
+    # Bar plot
+    plt.bar(0, np.sum(dico["fromGridPowers"]))
+    plt.bar(1, np.sum(dico["toGridPowers"]))
+    plt.xticks([0, 1], ["From Grid", "To Grid"])
+    plt.ylabel("Power (kW)")
+    plt.title("Proportion grid in and out")
+    plt.savefig(outputFolder + "/grid_in-out.png")
     plt.show()
 
+    # bar chart of the production vs the loads
     p1 = plt.bar(0, np.sum(dico["PVPowers"]), 0.2)
     p2 = plt.bar(0, np.sum(dico["windPowers"]), 0.2, bottom=np.sum(dico["PVPowers"]))
-    p3 = plt.bar(0, np.sum(dico["gridPowers"]), 0.2, bottom=np.sum(dico["windPowers"]) + np.sum(dico["PVPowers"]))
-    p4 = plt.bar(0, np.sum(dico["dieselGenerators"]), 0.2,
-                 bottom=np.sum(dico["windPowers"]) + np.sum(dico["PVPowers"]) + np.sum(dico["gridPowers"]))
+    p3 = plt.bar(0, np.sum(dico["fromGridPowers"]), 0.2, bottom=np.sum(dico["windPowers"]) + np.sum(dico["PVPowers"]))
+    p4 = plt.bar(0, np.sum(dico["dieselGenerators"]), 0.1,
+                 bottom=np.sum(dico["windPowers"]) + np.sum(dico["PVPowers"]) + np.sum(dico["fromGridPowers"]))
     p5 = plt.bar(0.7, np.sum(dico["fixedLoads"]), 0.2)
     p6 = plt.bar(0.7, np.sum(dico["batPowers"]), 0.2, bottom=np.sum(dico["fixedLoads"]))
     p7 = plt.bar(0.7, np.sum(dico["evPowers"]), 0.2, bottom=np.sum(dico["fixedLoads"]) + np.sum(dico["batPowers"]))
 
+    p8 = plt.bar(0.05, np.sum(dico["toGridPowers"]), 0.1,
+                 np.sum(dico["windPowers"]) + np.sum(dico["PVPowers"]) + np.sum(dico["fromGridPowers"]) - np.sum(
+                     dico["toGridPowers"]))
+
     plt.xticks([0, 0.7], ("Generator", "Loads"))
-    plt.legend((p1[0], p2[0], p3[0], p4[0], p5[0], p6[0], p7[0]),
-               ('PV power', 'wind Power', 'grid Power', 'diesel Power', 'fixed Loads', 'battery Power', 'EV power'))
+    plt.legend((p1[0], p2[0], p3[0], p4[0], p5[0], p6[0], p7[0], p8[0]),
+               ('PV power', 'wind Power', 'from grid Power', 'diesel Power', 'fixed Loads', 'battery Power', 'EV power',
+                'toGridPowers'))
     plt.show()
