4baf0830ab7e953079a3aec8620e783265911c7e
loulouagr <agrofoglio@telecom-paristech.fr>
2020-01-09 20:24:08 +0100

    adding the construction of the model inside a function for an easier use

--- code/forecasting.py
+++ code/forecasting.py
@@ -4,14 +4,11 @@ from keras.models import Sequential
 from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
 
+# fixing the random seed to have a better reproducibility
 seed = 3
 np.random.seed(seed)
 
-
-def one_hot_encode_object_array(arr):
-    return np_utils.to_categorical(arr)
-
-
+# input datas
 datas = np.array([[5.1, 3.5, 1.4, 0.2],
                   [4.9, 3., 1.4, 0.2],
                   [4.7, 3.2, 1.3, 0.2],
@@ -27,16 +24,33 @@ print(datas)
 
 train_X, test_X, train_y, test_y = train_test_split(datas, label, test_size=0.2, random_state=seed)
 
-train_y_ohe = one_hot_encode_object_array(train_y)
-test_y_ohe = one_hot_encode_object_array(test_y)
+train_y_ohe = np_utils.to_categorical(train_y)
+test_y_ohe = np_utils.to_categorical(test_y)
 
-model = Sequential()
 
-model.add(Dense(16, input_shape=(4,), activation='sigmoid'))
+# building the model
+def createModel(inputLayer, lLayer):
+    model = Sequential()
+    model.add(Dense(inputLayer[1], input_shape=inputLayer[0], activation='sigmoid'))
+    for foo in lLayer:
+        if foo[0] == 'D':
+            if foo[2] == "sig":
+                model.add(Dense(foo[1], activation='sigmoid'))
+            elif foo[2] == "soft":
+                model.add(Dense(foo[1], activation='softmax'))
 
-model.add(Dense(2, activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=["accuracy"])
+    return model
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=["accuracy"])
+
+model = createModel([(4,), 16], [('D', 16, 'sig'), ('D', 2, 'soft')])
+
+# training it
 model.fit(train_X, train_y_ohe, epochs=100, batch_size=1, verbose=0)
+
+# testing it
 loss, accuracy = model.evaluate(test_X, test_y_ohe, verbose=0)
 print("Accuracy = {:.2f}".format(accuracy))
+
+# plotting
+
