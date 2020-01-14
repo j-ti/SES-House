c974aeaf320701ff4b8ee49f9bb512680be36e91
loulouagr <agrofoglio@telecom-paristech.fr>
2020-01-14 12:01:03 +0100

    data preprocessing : building the train set

--- code/forecasting.py
+++ code/forecasting.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 import numpy as np
 from data import getNinjaPvApi
+from keras.layers import LSTM, Dropout, Activation
 from util import constructTimeStamps
+import pandas as pd
 
 from keras.layers.core import Dense
 from keras.models import Sequential
@@ -10,10 +12,13 @@ from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-
 # fixing the random seed to have a better reproducibility
 seed = 3
 np.random.seed(seed)
+
+# param
+look_back = 5  # we have a 5 point history in our input
+part = 0.6  # we train on part of the set
 
 # input datas : uncontrolable resource : solar production
 timestamps = constructTimeStamps(
@@ -22,36 +27,47 @@ timestamps = constructTimeStamps(
     datetime.strptime("01:00:00", "%H:%M:%S") - datetime.strptime("00:00:00", "%H:%M:%S")
 )
 
-metadata, df_pvPower = getNinjaPvApi(
+metadata, df = getNinjaPvApi(
     52.5170, 13.3889, timestamps
 )
 
-df_train = df_pvPower["electricity"][:600]
-df_train_label = df_pvPower["electricity"][1:601]
+split = int(len(df) * part)
+print(split)
 
-df_test = df_pvPower["electricity"][602:len(df_pvPower) -2]
-df_test_lebel = df_pvPower["electricity"][603:len(df_pvPower) -1]
+df_train = df["electricity"][look_back:split].reset_index(drop=True)
+for i in range(1, look_back):
+    s = df["electricity"][look_back - i:split-i].reset_index(drop=True)
+    df_train = pd.concat([df_train, s], axis=1, ignore_index=True)
 
+df_train_label = df["electricity"][look_back + 1:split + 1]
 
-model = Sequential()
-model.add(Dense(16, input_shape=(1,), activation='sigmoid'))
-model.add(Dense(15, activation='sigmoid'))
-model.add(Dense(1, activation='softmax'))
+print(df_train.shape)
+print(df_train_label.shape)
+assert len(df_train) == len(df_train_label)
 
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=["accuracy"])
+print(df_train)
 
-
-# training it
-model.fit(df_train, df_train_label, epochs=100, batch_size=1, verbose=0)
-
-# testing it
-loss, accuracy = model.evaluate(df_test, df_test_lebel, verbose=0)
-print("Accuracy = {:.2f}".format(accuracy))
-
-# plotting
-predict = model.predict(df_test)
-
-plt.plot(predict)
-plt.plot(df_test_lebel)
-
-plt.show()
+df_test = df["electricity"][602:len(df) - 2]
+df_test_lebel = df["electricity"][603:len(df) - 1]
+#
+# model = Sequential()
+# model.add(LSTM(256, input_shape=(1, look_back)))
+# model.add(Dropout(0.5))
+# model.add(Dense(1))
+# model.add(Activation('tanh'))
+# model.compile(loss='mean_squared_error', optimizer='adam')
+#
+# # training it
+# model.fit(trainx, trainy, epochs=20, batch_size=50, verbose=2)
+#
+# # testing it
+# loss, accuracy = model.evaluate(df_test, df_test_lebel, verbose=0)
+# print("Accuracy = {:.2f}".format(accuracy))
+#
+# # plotting
+# predict = model.predict(df_test)
+#
+# plt.plot(predict)
+# plt.plot(df_test_lebel)
+#
+# plt.show()
