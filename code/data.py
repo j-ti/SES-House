e5982f2cef25997cd530be30ac8e2e4547112bf1
jtm91 <jtm91@mailbox.tu-berlin.de>
2020-01-17 17:30:33 +0100

    little correction solar 2 nan values

--- code/data.py
+++ code/data.py
@@ -224,7 +224,8 @@ def resampleData(data, timestamps, offset=timedelta(days=0), positive=True):
     data = data.loc[timestamps[0] + offset : timestamps[-1] + offset]
     assert data.shape[1] <= 3
     if data.shape[1] == 3:
-        data[np.isnan(data)] = 0
+        not_numbers = np.isnan(data)
+        data.iloc[not_numbers] = 0
         dataOut = data.iloc[:, 0] + data.iloc[:, 1] + data.iloc[:, 2]
     else:
         dataOut = data.iloc[:, 0]
