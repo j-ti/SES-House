7371fb1bf9649433159eccda3454c6c6e7674a5f
Joris Clement <set.email@example.com>
2020-01-22 14:05:08 +0100

    Adapt config to what Louis and I agreed to, #87

---
+++ code/forecast_load_conf.py
@@ -0,0 +1,19 @@
+class ForecastLoadConfig:
+    def __init__(self):
+        self.DATA_FILE = "./data/15minute_data_newyork.csv"
+        self.TIME_HEADER = "local_15min"
+        self.DATAID = 1222
+        self.OUTPUT_FOLDER = "./output/forecast/load/"
+
+        self.LOAD_MODEL = False
+        self.MODEL_FILE = "./output/forecast/load/model.json"
+
+        self.EPOCHS = 30
+        self.LOOK_BACK = 15
+        self.BATCH_SIZE = 30
+        self.DROPOUT = 0.1
+        self.DENSE = 1
+        self.NEURONS = 8
+        self.ACTIVATION_FUNCTION = "relu"
+        self.LOSS_FUNCTION = "mean_squared_error"
+        self.OPTIMIZE_FUNCTION = "adam"
