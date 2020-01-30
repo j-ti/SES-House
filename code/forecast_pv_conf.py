2694d94f9f13e189ed29e60d498e38910a31cacc
loulouagr <agrofoglio@telecom-paristech.fr>
2020-01-30 18:38:46 +0100

    adding plot for validation test + adding nb of day to plot

--- code/forecast_pv_conf.py
+++ code/forecast_pv_conf.py
@@ -7,13 +7,13 @@ class ForecastPvConfig:
         self.DATE_START = "2019-05-01 00:00:00"
         self.DATE_END = "2019-06-01 00:00:00"
 
-        self.LOAD_MODEL = True
+        self.LOAD_MODEL = False
         self.MODEL_FILE = "./output/forecast/pv/model.json"
         self.MODEL_FILE_H5 = "./output/forecast/pv/model.h5"
 
         # TODO : for multiple layer of same type : put inside an array
         self.EPOCHS = 30
-        self.LOOK_BACK = 15
+        self.LOOK_BACK = 60
         self.BATCH_SIZE = 30
         self.DROPOUT = 0.1
         self.DENSE = 1
@@ -24,3 +24,5 @@ class ForecastPvConfig:
         self.ACTIVATION_FUNCTION = "relu"
         self.LOSS_FUNCTION = "mean_squared_error"
         self.OPTIMIZE_FUNCTION = "adam"
+
+        self.NB_PLOT = 6
