63fa86c30de1be2a85c8b741239d2188a408ad14
Joris Clement <j.clement@campus.tu-berlin.de>
2020-02-26 20:52:38 +0100

    Print test mse to know final test mse

--- code/get_best_model.py
+++ code/get_best_model.py
@@ -23,6 +23,7 @@ print(model_paths)
 
 best_model = ""
 best_value = 1.0
+best_test = 1.0
 
 for model_path in model_paths:
     model_path_h5 = model_path.replace("json", "h5")
@@ -33,19 +34,24 @@ for model_path in model_paths:
 
     loadConfig.LOOK_BACK = model.layers[0].input_shape[1]
 
-    _, validation_part, _, scaler = getNormalizedParts(
+    _, validation_part, test_part, scaler = getNormalizedParts(
         config, loadConfig, config.TIMESTAMPS
     )
     validation_x, validation_y = buildSet(
         validation_part, loadConfig.LOOK_BACK, loadConfig.OUTPUT_SIZE
     )
+    test_x, test_y = buildSet(test_part, loadConfig.LOOK_BACK, loadConfig.OUTPUT_SIZE)
 
     validation_prediction = model.predict(validation_x)
     validation_mse = mean_squared_error(validation_y, validation_prediction)
+    test_mse = mean_squared_error(test_y, model.predict(test_x))
+    print("test mse: ", test_mse)
     if validation_mse < best_value:
         best_value = validation_mse
+        best_test = test_mse
         best_model = model_path
     print("validation mse: ", validation_mse)
 
 print(best_model)
-print(best_value)
+print("best val mse: ", best_value)
+print("best test mse: ", best_test)
